e179dbbba8a4a35a7a4896c3ef1d142da55dcb52
Alphonso <alphonsotran@gmail.com>
2020-04-09 14:47:55 +0200

    Add function to add node at end of tail

--- singly_linked_list.js
+++ singly_linked_list.js
@@ -1,4 +1,22 @@
-function node(data) {
+function Node(data) {
   this.data = data;
   this.next = null;
+}
+
+class SinglyLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+  }
+
+  addNode(data) {
+    const newNode = new Node(data);
+    if (!this.head) {
+      this.head = node;
+      this.tail = node;
+    } else {
+      this.tail.next = node;
+      this.tail = node;
+    }
+  }
 }
